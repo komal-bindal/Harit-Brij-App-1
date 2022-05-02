fe807ac6d749ce21547e7977abe7db9bdab067f0
AyaanJaved <80831951+AyaanJaved@users.noreply.github.com>
2022-05-02 12:03:41 +0530

    Added maps SDK

--- app/src/main/java/com/haritbrij/haritBrij/MainActivity.java
+++ app/src/main/java/com/haritbrij/haritBrij/MainActivity.java
@@ -1,14 +1,71 @@
 package com.haritbrij.haritBrij;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = ".MainActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private TextView helloWorldTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloWorldTextView = findViewById(R.id.hello_world);
+
+        helloWorldTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+//        createAccount("asdf@gmail.com", "123456");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mUser = mAuth.getCurrentUser();
+        helloWorldTextView.setText(mUser.getEmail());
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            mUser = mAuth.getCurrentUser();
+                            if ( mUser != null ) {
+                                helloWorldTextView.setText(mUser.getEmail());
+                            }
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, String.valueOf(task.getException()));
+                        }
+                    }
+                });
     }
 }
