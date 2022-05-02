dc0d0e6f4569296223aec1ce8437347d04636f7c
AyaanJaved <80831951+AyaanJaved@users.noreply.github.com>
2022-05-02 18:43:20 +0530

    Added UserTypeFragment

---
+++ app/src/main/java/com/haritbrij/haritBrij/UserTypeFragment.java
@@ -0,0 +1,9 @@
+package com.haritbrij.haritBrij;
+
+import androidx.fragment.app.Fragment;
+
+public class UserTypeFragment extends Fragment {
+    UserTypeFragment() {
+        super(R.layout.fragment_user_type);
+    }
+}
