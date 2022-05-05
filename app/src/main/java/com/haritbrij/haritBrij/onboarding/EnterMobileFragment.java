a72a3c8bdb25f7a7d85ce81993a572a31bf36842
AyaanJaved <80831951+AyaanJaved@users.noreply.github.com>
2022-05-05 21:34:43 +0530

    added send otp button listener

--- app/src/main/java/com/haritbrij/haritBrij/onboarding/EnterMobileFragment.java
+++ app/src/main/java/com/haritbrij/haritBrij/onboarding/EnterMobileFragment.java
@@ -1,5 +1,12 @@
 package com.haritbrij.haritBrij.onboarding;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.haritbrij.haritBrij.R;
@@ -7,5 +14,22 @@ import com.haritbrij.haritBrij.R;
 public class EnterMobileFragment extends Fragment {
     EnterMobileFragment() {
         super(R.layout.fragment_enter_mobile);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText mobileNumberEditText = view.findViewById(R.id.mobile_num_edit_text);
+        Button sendOtpButton = view.findViewById(R.id.send_otp_button);
+
+        sendOtpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enteredMobileNumber = mobileNumberEditText.getText().toString();
+
+                //call the api passing the enteredMobileNumber.
+            }
+        });
     }
 }
