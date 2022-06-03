3f661483728300f6a50070c57fea9a3c1af76664
TanyaAgarwal28 <tanya.agrawal1_cs19@gla.ac.in>
2022-06-03 11:21:06 +0530

    Added user panel screens

---
+++ app/src/main/java/com/haritbrij/haritBrij/AdminLoginFragment.java
@@ -0,0 +1,14 @@
+package com.haritbrij.haritBrij;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminLoginFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login_fragment);
+    }
+}
